19f7f82d078fd66ef62dc269d0dcd1be7e7618e3
zaimont <montserratlozano03@gmail.com>
2025-07-17 04:08:59 -0600

    Actualización con deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // 👈 ESTA LÍNEA ES CLAVE
+  base: '/portafolio2.1/',
   plugins: [react(), tailwindcss()],
 })
